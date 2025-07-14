4df56f442860d2a6edc0f73d8e6c23448731e0c5
Kandler3 <artchuk@icloud.com>
2025-07-14 19:31:31 +0300

    cfg changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tutor-landing/',
   server: {
     host: true,
     port: 5173,
